a57f3f0147cef87afda346ad985d46de2f69478f
An2468 <105502778+An2468@users.noreply.github.com>
2022-06-10 21:03:32 +0700

    Update PingPong.py

--- PingPong.py
+++ PingPong.py
@@ -1,6 +1,10 @@
 from pygame import *
 
-window = display.set_mode((700, 600))
+WIDTH = 700
+HEIGHT = 600
+
+window = display.set_mode((WIDTH, HEIGHT))
+
 
 clock = time.Clock()
 
@@ -43,17 +47,24 @@ class Player1(ImageSprite):
             self.rect.top = 500
         if self.rect.bottom <= 100:
             self.rect.bottom = 100
+        
+
 
 class Ball(ImageSprite):
     def update(self):
+        if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
+            self.speed.y *= -1
         self.rect.topleft += self.speed
 
          
         
 
-player_1 = Player1(file="rocket.png", position=(0, 510), size=(80, 80), speed=(0, 12.5))
-player_2 = Player2(file="rocket.png", position=(620, 510), size=(80, 80), speed=(0, 12.5))
-ball = Ball(file="ball.png", position=(250, 250), size=(80, 80), speed=(2, 3))
+player_1 = Player1(file="PingPong.png", position=(0, 510), size=(80, 80), speed=(0, 12.5))
+player_2 = Player2(file="PingPongBlue.png", position=(620, 510), size=(80, 80), speed=(0, 12.5))
+ball = Ball(file="ball.png", position=(100, 100), size=(30, 30), speed=(2, 3))
+red_win = ImageSprite(file="RedTeam.png", position=(0, 0), size=(WIDTH, HEIGHT), speed=0)
+blue_win = ImageSprite(file="BlueTeam.png", position=(0, 0), size=(WIDTH, HEIGHT), speed=0)
+
 
 
 
@@ -67,7 +78,7 @@ while game_on:
 
         if ev.type == QUIT:
             game_on = False
-
+    
     window.fill((255, 255, 255))
     player_1.update()
     player_1.draw(window)
@@ -75,6 +86,19 @@ while game_on:
     player_2.draw(window)
     ball.update()
     ball.draw(window)
+    
+    if sprite.collide_rect(ball, player_1) or sprite.collide_rect(ball, player_2):
+        ball.speed.x *= -1
+        ball.rect.x += ball.speed.x
+    
+    if ball.rect.x <= 0:
+        blue_win.draw(window)
+        
+ 
+
+    if ball.rect.right >= WIDTH:
+        red_win.draw(window)
+
 
     display.update()
     clock.tick(69)
